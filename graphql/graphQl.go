c3e423722e86bf1eb67970ff97f5f77cca81839a
IPreferWater <ipreferwater2@gmail.com>
2020-03-06 17:51:53 +0100

    prepare example of structure to start working with files

---
+++ graphql/graphQl.go
@@ -0,0 +1,105 @@
+package graphql
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/graphql-go/graphql"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+
+var data map[string]user
+
+var schema, _ = graphql.NewSchema(
+	graphql.SchemaConfig{
+		Query: queryType,
+	},
+)
+
+var userType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
+var queryType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Query",
+		Fields: graphql.Fields{
+			"user": &graphql.Field{
+				Type: userType,
+				Args: graphql.FieldConfigArgument{
+					"id": &graphql.ArgumentConfig{
+						Type: graphql.String,
+					},
+				},
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					idQuery, isOK := p.Args["id"].(string)
+					if isOK {
+						return data[idQuery], nil
+					}
+					return nil, nil
+				},
+			},
+		},
+	})
+
+func importJSONDataFromFile(fileName string, result interface{}) (isOK bool) {
+	isOK = true
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Print("Error:", err)
+		isOK = false
+	}
+	err = json.Unmarshal(content, result)
+	if err != nil {
+		isOK = false
+		fmt.Print("Error:", err)
+	}
+	return
+}
+
+func executeQuery(query string, schema graphql.Schema) *graphql.Result {
+	result := graphql.Do(graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	})
+	if len(result.Errors) > 0 {
+		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
+	}
+	return result
+}
+
+func StartServerGraphQL(){
+	//graphql
+	_ = importJSONDataFromFile("data.json", &data)
+
+	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		result := executeQuery(r.URL.Query().Get("query"), schema)
+		json.NewEncoder(w).Encode(result)
+	})
+
+	fmt.Println("Now server is running on port 8081")
+	fmt.Println("Test with Get      : curl -g 'http://localhost:8081/graphql?query={user(id:\"1\"){name}}'")
+
+
+	err2 := http.ListenAndServe(":8081", nil)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+}
--- main.go
+++ main.go
@@ -1,115 +1,45 @@
 package main
 
 import (
-	"encoding/json"
+	"./graphql"
 	"fmt"
-	"github.com/graphql-go/graphql"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-type user struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
-
-var data map[string]user
-
-var schema, _ = graphql.NewSchema(
-	graphql.SchemaConfig{
-		Query: queryType,
-	},
-)
-
-var userType = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "User",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
-		},
-	},
-)
-
-var queryType = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "Query",
-		Fields: graphql.Fields{
-			"user": &graphql.Field{
-				Type: userType,
-				Args: graphql.FieldConfigArgument{
-					"id": &graphql.ArgumentConfig{
-						Type: graphql.String,
-					},
-				},
-				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-					idQuery, isOK := p.Args["id"].(string)
-					if isOK {
-						return data[idQuery], nil
-					}
-					return nil, nil
-				},
-			},
-		},
-	})
 
 func main() {
-	//logfile server
-	fs := http.FileServer(http.Dir("./stock"))
-	http.Handle("/", fs)
+	testDirectory()
 
-	log.Println("Listening on :8080...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		graphql.StartServerGraphQL()
+	}()
 
-	//graphql
-	_ = importJSONDataFromFile("data.json", &data)
+		fs := http.FileServer(http.Dir("./stock"))
+		http.Handle("/", fs)
 
-	http.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
-		result := executeQuery(r.URL.Query().Get("query"), schema)
-		json.NewEncoder(w).Encode(result)
+		log.Println("Listening on :8080...")
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+}
+
+func testDirectory(){
+	var files []string
+
+	root := "./stock"
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
 	})
-
-	fmt.Println("Now server is running on port 8081")
-	fmt.Println("Test with Get      : curl -g 'http://localhost:8081/graphql?query={user(id:\"1\"){name}}'")
-	err2 := http.ListenAndServe(":8081", nil)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-
-
-}
-
-func importJSONDataFromFile(fileName string, result interface{}) (isOK bool) {
-	isOK = true
-	content, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		fmt.Print("Error:", err)
-		isOK = false
+		panic(err)
 	}
-	err = json.Unmarshal(content, result)
-	if err != nil {
-		isOK = false
-		fmt.Print("Error:", err)
+	for _, file := range files {
+		fmt.Println(file)
 	}
-	return
 }
 
-func executeQuery(query string, schema graphql.Schema) *graphql.Result {
-	result := graphql.Do(graphql.Params{
-		Schema:        schema,
-		RequestString: query,
-	})
-	if len(result.Errors) > 0 {
-		fmt.Printf("wrong result, unexpected errors: %v", result.Errors)
-	}
-	return result
-}
