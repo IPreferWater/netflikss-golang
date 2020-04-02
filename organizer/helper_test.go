11bc03ccf2c17bbc0a74636fea15004d8bd5ad69
IPreferWater <ipreferwater2@gmail.com>
2020-04-02 16:07:05 +0200

    prepare test to guess suffix of files

--- organizer/helper_test.go
+++ organizer/helper_test.go
@@ -4,14 +4,22 @@ import "testing"
 
 func TestGuessNumber(t *testing.T) {
 	shouldBeOne := guessNumber("random1.xxx")
-
 	if shouldBeOne != "1" {
-		t.Errorf("Sum was incorrect, got: %s, want: %s", shouldBeOne, "1")
+		t.Errorf("incorrect, got: %s, want: %s", shouldBeOne, "1")
 	}
 
-	guessNumber("random01")
+	shouldBeOneWithoutZero := guessNumber("random01.xxx")
+	if shouldBeOneWithoutZero != "1" {
+		t.Errorf("incorrect, got: %s, want: %s", shouldBeOneWithoutZero, "1")
+	}
 
-	guessNumber("random")
+	shouldBeEmpty := guessNumber("random.xxx")
+	if shouldBeEmpty != "" {
+		t.Errorf("incorrect, got: %s, want: %s", shouldBeEmpty, "empty string")
+	}
 
-	guessNumber("random10")
+	shouldBe999 := guessNumber("random000000000999")
+	if shouldBe999 != "999" {
+		t.Errorf("incorrect, got: %s, want: %s", shouldBe999, "999")
+	}
 }
