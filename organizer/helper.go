abe72660507c5423a5cdc24f18405c9c8a41f781
IPreferWater <ipreferwater2@gmail.com>
2020-04-06 16:07:24 +0200

    can create info.json, still have somme issues inside

--- organizer/helper.go
+++ organizer/helper.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	stockPath        string = "stock"
+	stockPath        string = "../stock"
 	infoJSONFileName string = "info.json"
 )
 
@@ -27,14 +27,29 @@ func fileExists(filename string) bool {
 	return !info.IsDir()
 }
 
-//TODO: we need to let the user choose his media folder
+//TODO: we need to delete this method
 func getAllInStockFolder() []os.FileInfo {
 	files, err := ioutil.ReadDir(stockPath)
 	if err != nil {
-		fmt.Printf("***\ncan't read the file %s \n", stockPath)
 		log.Fatal(err)
 	}
 	return files
+}
+
+func getAllFiles(path string) []os.FileInfo {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return files
+}
+
+func getAllDirectories(path string) []os.FileInfo {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filterByDirectory(files)
 }
 
 func filterByDirectory(files []os.FileInfo) []os.FileInfo {
@@ -48,7 +63,7 @@ func filterByDirectory(files []os.FileInfo) []os.FileInfo {
 	return directories
 }
 
-//work method
+//ReadAllInside read all info.json files
 func ReadAllInside() []model.Serie {
 	files := getAllInStockFolder()
 	filtered := filterByDirectory(files)
@@ -56,7 +71,6 @@ func ReadAllInside() []model.Serie {
 
 	for _, directory := range filtered {
 		infoJSONPath := filepath.Join(stockPath, directory.Name(), infoJSONFileName)
-		fmt.Printf("in the directory %s the info.json exit ? %t\n", directory.Name(), fileExists(infoJSONPath))
 		if fileExists(infoJSONPath) {
 			content, err := ioutil.ReadFile(infoJSONPath)
 			if err != nil {
@@ -71,6 +85,73 @@ func ReadAllInside() []model.Serie {
 		}
 	}
 	return series
+}
+
+//BuildInfoJSONFile build info.json files
+func BuildInfoJSONFile(){
+	directories := getAllDirectories(stockPath)
+
+	for _, directory := range directories {
+		infoJSONPath := filepath.Join(stockPath, directory.Name(), infoJSONFileName)
+		serieToCreate := model.Serie{
+			DirectoryName: directory.Name(),
+			Label: directory.Name(),
+		}
+
+		if !fileExists(infoJSONPath) {
+			seasonDirPath := filepath.Join(stockPath, directory.Name())
+
+			seasonsDirs := getAllDirectories(seasonDirPath)
+			seasonsToCreate := make([]*model.Season, len(seasonsDirs))
+
+			for _, seasonDir := range seasonsDirs {
+				fileName := seasonDir.Name()
+
+				//fmt.Printf("? %s \n",seasonDir.Name())
+				episodesPath := filepath.Join(stockPath, directory.Name(), fileName)
+				episodes := getAllFiles(episodesPath)
+				episodeCreated := createAllEpisode(episodes)
+
+				guessNumber := guessNumber(fileName)
+				number, _ := strconv.Atoi(guessNumber)
+
+				//TODO: do we really need a label on the season ?
+				newSeason := model.Season{
+					DirectoryName: seasonDir.Name(),
+					Number: number,
+					Label: guessNumber,
+					Episodes: episodeCreated,
+				}
+				seasonsToCreate = append(seasonsToCreate, &newSeason)
+
+			}
+
+			serieToCreate.Seasons=seasonsToCreate
+
+			file, _ := json.MarshalIndent(serieToCreate, "", " ")
+			_ = ioutil.WriteFile("info.json", file, 0644)
+		}
+	}
+}
+
+func createAllEpisode(episodes []os.FileInfo) []*model.Episode{
+	episodesToCreate := make([]*model.Episode, len(episodes))
+	for _, episode := range episodes {
+		//fmt.Printf("? %s \n",episode.Name())
+		fileName := episode.Name()
+		
+		guessNumber := guessNumber(fileName)
+		number, _ := strconv.Atoi(guessNumber)
+
+		//TODO: we should initialize a label without extenstion
+		newEpisode := model.Episode{
+			FileName: fileName,
+			Number: number,
+			Label: fileName,
+		}
+		episodesToCreate = append(episodesToCreate, &newEpisode)
+	}
+	return episodesToCreate
 }
 
 func isNumeric(s string) bool {
