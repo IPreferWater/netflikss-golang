8125ca4101c631de5c796cdda28b824402e60d57
IPreferWater <ipreferwater2@gmail.com>
2020-04-10 16:16:21 +0200

    remove initializing array with lenght in createJson as it add null values

--- organizer/infojson_creation.go
+++ organizer/infojson_creation.go
@@ -6,24 +6,26 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+
 	"github.com/ipreferwater/netflikss-golang/graph/model"
 )
+
 //BuildInfoJSONFile build info.json files
-func BuildInfoJSONFile(){
+func BuildInfoJSONFile() {
 	directories := getAllDirectories(stockPath)
 
 	for _, directory := range directories {
 		infoJSONPath := filepath.Join(stockPath, directory.Name(), infoJSONFileName)
 		serieToCreate := model.Serie{
 			DirectoryName: directory.Name(),
-			Label: directory.Name(),
+			Label:         directory.Name(),
 		}
 
 		if !fileExists(infoJSONPath) {
 			seasonDirPath := filepath.Join(stockPath, directory.Name())
 
 			seasonsDirs := getAllDirectories(seasonDirPath)
-			seasonsToCreate := make([]*model.Season, len(seasonsDirs))
+			seasonsToCreate := make([]*model.Season, 0)
 
 			for _, seasonDir := range seasonsDirs {
 				fileName := seasonDir.Name()
@@ -39,15 +41,15 @@ func BuildInfoJSONFile(){
 				//TODO: do we really need a label on the season ?
 				newSeason := model.Season{
 					DirectoryName: seasonDir.Name(),
-					Number: number,
-					Label: guessNumber,
-					Episodes: episodeCreated,
+					Number:        number,
+					Label:         guessNumber,
+					Episodes:      episodeCreated,
 				}
 				seasonsToCreate = append(seasonsToCreate, &newSeason)
 
 			}
 
-			serieToCreate.Seasons=seasonsToCreate
+			serieToCreate.Seasons = seasonsToCreate
 
 			file, _ := json.MarshalIndent(serieToCreate, "", " ")
 			_ = ioutil.WriteFile(infoJSONPath, file, 0644)
@@ -55,21 +57,20 @@ func BuildInfoJSONFile(){
 	}
 }
 
-
-func createAllEpisode(episodes []os.FileInfo) []*model.Episode{
-	episodesToCreate := make([]*model.Episode, len(episodes))
+func createAllEpisode(episodes []os.FileInfo) []*model.Episode {
+	episodesToCreate := make([]*model.Episode, 0)
 	for _, episode := range episodes {
 		//fmt.Printf("? %s \n",episode.Name())
 		fileName := episode.Name()
-		
+
 		guessNumber := guessNumber(fileName)
 		number, _ := strconv.Atoi(guessNumber)
 
 		//TODO: we should initialize a label without extenstion
 		newEpisode := model.Episode{
 			FileName: fileName,
-			Number: number,
-			Label: fileName,
+			Number:   number,
+			Label:    fileName,
 		}
 		episodesToCreate = append(episodesToCreate, &newEpisode)
 	}
