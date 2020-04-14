8c549fa37f051d37f3939b7d670ead2b04a52344
IPreferWater <ipreferwater2@gmail.com>
2020-04-14 11:18:54 +0200

    rename globalVariable to stockPath & fileServerPath

--- server.go
+++ server.go
@@ -34,9 +34,15 @@ func main() {
 	//init the path from config
 	configuration := configuration.ReadConfigurationFile()
 	if configuration.StockPath == "" {
-		organizer.CurrentStockPath = user.HomeDir
-	}else {
-		organizer.CurrentStockPath = configuration.StockPath
+		organizer.StockPath = user.HomeDir
+	} else {
+		organizer.StockPath = configuration.StockPath
+	}
+
+	if configuration.FileServerPath == "" {
+		organizer.FileServerPath = user.HomeDir
+	} else {
+		organizer.FileServerPath = configuration.FileServerPath
 	}
 
 
@@ -48,7 +54,7 @@ func main() {
 	http.Handle("/playground", c.Handler(playground.Handler("GraphQL playground", "/query")))
 	http.Handle("/query", c.Handler(srv))
 	http.Handle("/usb", http.FileServer(http.Dir("/dev")))
-	http.Handle("/", http.FileServer(http.Dir(user.HomeDir)))
+	http.Handle("/", http.FileServer(http.Dir(configuration.FileServerPath)))
 	http.HandleFunc("/stockpath", stockPath)
 	http.HandleFunc("/directorieslist", directoriesList)
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
@@ -69,9 +75,10 @@ func stockPath(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		/*configuration := configuration.GetConfigurationByteFormat()
 		w.Write(configuration)*/
-		w.Write([]byte(organizer.CurrentStockPath))
+		w.Write([]byte(organizer.StockPath))
 
 	case "POST":
+		//TODO: we update just the config.json, not the global variable
 		newConfiguration := configuration.Configuration{}
 		err := json.NewDecoder(r.Body).Decode(&newConfiguration)
 		if err != nil {
@@ -94,7 +101,7 @@ func directoriesList(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	path := filepath.Join(organizer.CurrentStockPath, s)
+	path := filepath.Join(organizer.StockPath, s)
 	println(path)
 
 	 listDirectoriesName := organizer.GetAllDirectoriesName(path)
