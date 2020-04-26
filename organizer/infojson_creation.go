4ba30df5bb7a34ba241598d0756e2668406aef9e
IPreferWater <ipreferwater2@gmail.com>
2020-04-26 11:04:50 +0200

    feat(movie): try to guess if movie or serie in creationInfoJson & improve code

--- organizer/infojson_creation.go
+++ organizer/infojson_creation.go
@@ -2,6 +2,7 @@ package organizer
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -18,51 +19,36 @@ func BuildInfoJSONFile() {
 	directories := getAllDirectories(path)
 
 	for _, directory := range directories {
-		infoJSONPath := filepath.Join(path, directory.Name(), infoJSONFileName)
+		directoryPath := filepath.Join(path, directory.Name())
+		infoJSONPath := filepath.Join(directoryPath, infoJSONFileName)
 
 		if !fileExists(infoJSONPath) {
 
-			seasonDirPath := filepath.Join(path, directory.Name())
+			allFiles := getAllFiles(directoryPath)
 
-			allFiles := getAllFiles(seasonDirPath)
-			seasonsDirs := filterByDirectory(allFiles)
+			guessType := guessType(allFiles)
 
-			img := findImage(allFiles)
-			serieToCreate := model.Serie{
-				DirectoryName: directory.Name(),
-				Label:         directory.Name(),
-				StockPath:     di.Configuration.StockPath,
-				Img:           img,
+			if guessType == "serie" {
+				createSerie(allFiles, directory, infoJSONPath, directoryPath)
+			} else if guessType == "movie" {
+				fmt.Println("need to create a movie")
 			}
 
-			seasonsToCreate := make([]*model.Season, 0)
-
-			for _, seasonDir := range seasonsDirs {
-				fileName := seasonDir.Name()
-
-				episodesPath := filepath.Join(path, directory.Name(), fileName)
-				episodes := getAllFiles(episodesPath)
-				episodeCreated := createAllEpisode(episodes)
-
-				guessNumber := guessNumber(fileName)
-				number, _ := strconv.Atoi(guessNumber)
-
-				newSeason := model.Season{
-					DirectoryName: seasonDir.Name(),
-					Number:        number,
-					Label:         guessNumber,
-					Episodes:      episodeCreated,
-				}
-				seasonsToCreate = append(seasonsToCreate, &newSeason)
-
-			}
-
-			serieToCreate.Seasons = seasonsToCreate
-
-			file, _ := json.MarshalIndent(serieToCreate, "", " ")
-			_ = ioutil.WriteFile(infoJSONPath, file, 0644)
 		}
 	}
+}
+
+func guessType(files []os.FileInfo) string {
+	//TODO: make an enum with iota
+
+	for _, file := range files {
+		ext := filepath.Ext(file.Name())
+		if ext == ".mp4" || ext == ".MP4" {
+			return "movie"
+		}
+	}
+
+	return "serie"
 }
 
 func findImage(files []os.FileInfo) string {
@@ -72,6 +58,53 @@ func findImage(files []os.FileInfo) string {
 		return ""
 	}
 	return allImages[0].Name()
+}
+
+func createSerie(files []os.FileInfo, directory os.FileInfo, infoJSONPath string, directoryPath string) {
+
+	seasonsDirs := filterByDirectory(files)
+
+	img := findImage(files)
+	serieToCreate := model.Serie{
+		DirectoryName: directory.Name(),
+		Label:         directory.Name(),
+		StockPath:     di.Configuration.StockPath,
+		Img:           img,
+	}
+
+	seasonsToCreate := make([]*model.Season, 0)
+
+	for _, seasonDir := range seasonsDirs {
+		fileName := seasonDir.Name()
+
+		episodesPath := filepath.Join(directoryPath, fileName)
+		episodes := getAllFiles(episodesPath)
+		episodeCreated := createAllEpisode(episodes)
+
+		guessNumber := guessNumber(fileName)
+		number, _ := strconv.Atoi(guessNumber)
+
+		newSeason := model.Season{
+			DirectoryName: seasonDir.Name(),
+			Number:        number,
+			Label:         guessNumber,
+			Episodes:      episodeCreated,
+		}
+		seasonsToCreate = append(seasonsToCreate, &newSeason)
+
+	}
+
+	serieToCreate.Seasons = seasonsToCreate
+
+	writeInfoJSONFile(serieToCreate, infoJSONPath)
+	/*file, _ := json.MarshalIndent(serieToCreate, "", " ")
+	_ = ioutil.WriteFile(infoJSONPath, file, 0644)*/
+
+}
+
+func writeInfoJSONFile(object interface{}, infoJSONPath string) {
+	file, _ := json.MarshalIndent(object, "", " ")
+	_ = ioutil.WriteFile(infoJSONPath, file, 0644)
 }
 
 func createAllEpisode(episodes []os.FileInfo) []*model.Episode {
